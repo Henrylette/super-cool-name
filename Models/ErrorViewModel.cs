c60eca7167c7c0b10500472c444506afbe40e5ec
henrietteabbednaes <henr8588@edu.ucl.dk>
2022-06-06 12:57:16 +0200

    started project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace super_cool_name.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
